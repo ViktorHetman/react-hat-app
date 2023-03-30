7f4a569f7260d6b222485556ce0ad52ec38fc561
Viktor Hetman <vitya.getman.79@gmail.com>
2023-03-30 13:25:13 +0300

    creating interceptor for axios instance to refresh

--- src/services/getTokens.js
+++ src/services/getTokens.js
@@ -21,23 +21,3 @@ export const getTokens = createAsyncThunk('tokens/getTokens', async (user) => {
   }
   return token
 })
-
-export const refreshTokens = createAsyncThunk(
-  'tokens/refreshTokens',
-  async (token) => {
-    const res = await axios.post(`${WEB_URL}/refresh`, {
-      headers: {
-        Refresh: token.refreshToken,
-        Accept: 'application/json',
-      },
-    })
-    const newTokens = {
-      accessToken: res.data.data.accessToken,
-      accessTokenEndTime: res.data.data.accessTokenEndTime,
-      cabinetUserId: res.data.data.cabinetUserId,
-      refreshToken: res.data.data.refreshToken,
-      refreshTokenEndTime: res.data.data.refreshTokenEndTime,
-    }
-    return newTokens
-  }
-)
